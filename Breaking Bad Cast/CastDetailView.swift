dfd54c93f72a9d2dc1165bafbe584d4ae7ceb4f3
Timothy Waters <timothy.waters@gmail.com>
2022-03-15 16:59:29 -0500

    March 15, 2022

--- Breaking Bad Cast/CastDetailView.swift
+++ Breaking Bad Cast/CastDetailView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct CastDetailView: View {
+    var character: Character
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(character.img)
+            Text(character.name)
+        }
     }
 }
 
 struct CastDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        CastDetailView()
+        CastDetailView(character: ViewModel().characters.first!)
     }
 }
